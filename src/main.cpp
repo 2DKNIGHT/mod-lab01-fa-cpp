dc69ba246012073dbf4e82caf590704c2e06e435
2DKNIGHT <93935170+2DKNIGHT@users.noreply.github.com>
2022-04-29 21:50:10 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
+#include <iostream>
 
 int main() {
-    return 0;
+    const char* str = "Something like! a dra1 or   o ";
+    std::cout << faStr1(str);
+    std::cout << faStr2(str);
+    std::cout << faStr3(str);
 }
